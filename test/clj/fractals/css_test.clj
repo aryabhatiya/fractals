f533a705eb4ed16dbf7987f2cadc11dfdbdbb718
Aryabhatiya Algebra <aryabhatiya.algebra@gmail.com>
2017-09-17 19:00:07 +0600

    java interop

---
+++ test/clj/fractals/css_test.clj
@@ -0,0 +1,19 @@
+(ns fractals.css-test
+  (:require [clojure.test :refer [deftest is]])
+  (:import [java.io StringReader]
+           [com.steadystate.css.parser CSSOMParser SACParserCSS3]
+           [com.steadystate.css.dom CSSStyleSheetImpl]
+           [com.steadystate.css.format CSSFormat]
+           [org.w3c.css.sac InputSource]
+           ))
+
+(deftest cssparse
+  (let [input-source (InputSource. (StringReader. "h1 { background: #ffcc44; }"))
+        parser (CSSOMParser. (SACParserCSS3. ))]
+    (is (= "h1 { background: #ffcc44 }"
+           (.getCssText (.parseStyleSheet parser input-source nil nil)
+                        (.setRgbAsHex (CSSFormat. ) true))))
+    (= (first (for [meth  (.getMethods CSSOMParser)
+                    :let [name2 (.getName meth)]
+                    :when (re-find #"parse" name2)]
+                name2)) "parseStyleSheet")))
